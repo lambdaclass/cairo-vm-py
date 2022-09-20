98ecdb29b9e413904d21def253210fbd6c6ef374
Federica Moletta <fedemoletta@hotmail.com>
2022-09-20 17:53:52 -0300

    Add ap & fp

--- src/execute_hint.rs
+++ src/execute_hint.rs
@@ -1,6 +1,6 @@
 use pyo3::PyCell;
 use std::rc::Rc;
-use crate::utils::to_vm_error;
+use crate::{utils::to_vm_error, relocatable::PyRelocatable};
 use cairo_rs::{hint_processor::{builtin_hint_processor::builtin_hint_processor_definition::HintProcessorData, proxies::{vm_proxy::VMProxy, exec_scopes_proxy::ExecutionScopesProxy}}, vm::errors::vm_errors::VirtualMachineError};
 use pyo3::{Python, types::PyDict};
 
@@ -21,8 +21,12 @@ impl PythonExecutor {
                 let memory = PyMemory { memory: Rc::clone(&vm_proxy.memory)};
                 let segments = PySegmentManager { segment_manager: Rc::clone(&vm_proxy.segments), memory: Rc::clone(&vm_proxy.memory) };
                 let globals = PyDict::new(py);
+                let ap =  PyRelocatable::new((1, vm_proxy.run_context.ap));
+                let fp =  PyRelocatable::new((1, vm_proxy.run_context.fp));
                 globals.set_item("memory", pycell!(py, memory)).map_err(to_vm_error)?;
                 globals.set_item("segments", pycell!(py, segments)).map_err(to_vm_error)?;
+                globals.set_item("ap", pycell!(py, ap)).map_err(to_vm_error)?;
+                globals.set_item("fp", pycell!(py, fp)).map_err(to_vm_error)?;
                 py.run(&hint_data.code, Some(globals), None).map_err(to_vm_error)?;
                 Ok(())
         })?;
