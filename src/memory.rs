ae0a79ef803d7f56c1c2f95deee06d5abf1fa420
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2022-09-28 13:04:31 -0300

    Add unit tests for pymemory and pysegmentmanager

--- src/memory.rs
+++ src/memory.rs
@@ -2,17 +2,18 @@ use crate::{
     relocatable::{PyMaybeRelocatable, PyRelocatable},
     vm_core::PyVM,
 };
-use cairo_rs::{types::relocatable::{MaybeRelocatable, Relocatable}, vm::vm_core::VirtualMachine};
-use num_bigint::BigInt;
+use cairo_rs::{
+    types::relocatable::{MaybeRelocatable, Relocatable},
+    vm::vm_core::VirtualMachine,
+};
 use pyo3::{
-    exceptions::{PyKeyError, PyTypeError, PyValueError},
+    exceptions::{PyTypeError, PyValueError},
     prelude::*,
 };
 use std::{cell::RefCell, rc::Rc};
 
 const MEMORY_GET_ERROR_MSG: &str = "Failed to get value from Cairo memory";
 const MEMORY_SET_ERROR_MSG: &str = "Failed to set value to Cairo memory";
-const MEMORY_SET_TYPE_ERROR_MSG: &str = "Failed to set downcast Python value";
 
 #[pyclass(unsendable)]
 pub struct PyMemory {
@@ -28,8 +29,15 @@ impl PyMemory {
 
     #[getter]
     pub fn __getitem__(&self, key: &PyRelocatable, py: Python) -> PyResult<Option<PyObject>> {
-        let key = key.to_relocatable();
-        match self.vm.borrow().memory.get(&key).map_err(|_| PyTypeError::new_err(MEMORY_GET_ERROR_MSG))? {
+        let key: Relocatable = key.into();
+
+        match self
+            .vm
+            .borrow()
+            .memory
+            .get(&key)
+            .map_err(|_| PyTypeError::new_err(MEMORY_GET_ERROR_MSG))?
+        {
             Some(maybe_reloc) => Ok(Some(PyMaybeRelocatable::from(maybe_reloc).to_object(py))),
             None => Ok(None),
         }
@@ -37,21 +45,24 @@ impl PyMemory {
 
     #[setter]
     pub fn __setitem__(&self, key: &PyRelocatable, value: PyMaybeRelocatable) -> PyResult<()> {
+        let key: Relocatable = key.into();
+        let value: MaybeRelocatable = value.into();
+
         self.vm
             .borrow_mut()
             .memory
-            .insert_value(&Into::<Relocatable>::into(key), Into::<MaybeRelocatable>::into(value))
+            .insert_value(&key, value)
             .map_err(|_| PyValueError::new_err(MEMORY_SET_ERROR_MSG))
     }
 }
 
 #[cfg(test)]
 mod test {
-    use num_bigint::{BigInt, Sign};
-    use pyo3::{Python, types::PyDict};
-    use pyo3::PyCell;
-    use crate::{PyVM, memory::PyMemory, relocatable::PyRelocatable, pycell};
     use crate::utils::to_vm_error;
+    use crate::{memory::PyMemory, relocatable::PyRelocatable, PyVM};
+    use num_bigint::{BigInt, Sign};
+    use pyo3::PyCell;
+    use pyo3::{types::PyDict, Python};
 
     #[test]
     fn memory_insert_test() {
@@ -67,8 +78,12 @@ mod test {
             let ap = PyRelocatable::from(vm.vm.borrow().get_ap());
 
             let globals = PyDict::new(py);
-            globals.set_item("memory", PyCell::new(py, memory).unwrap()).unwrap();
-            globals.set_item("ap", PyCell::new(py, ap).unwrap()).unwrap();
+            globals
+                .set_item("memory", PyCell::new(py, memory).unwrap())
+                .unwrap();
+            globals
+                .set_item("ap", PyCell::new(py, ap).unwrap())
+                .unwrap();
 
             let code = "memory[ap] = 5";
 
@@ -78,7 +93,40 @@ mod test {
         });
     }
 
-        #[test]
+    #[test]
+    fn memory_insert_ocuppied_address_error_test() {
+        Python::with_gil(|py| {
+            let vm = PyVM::new(
+                BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+                false,
+            );
+            for _ in 0..2 {
+                vm.vm.borrow_mut().add_memory_segment();
+            }
+            let memory = PyMemory::new(&vm);
+            let ap = PyRelocatable::from(vm.vm.borrow().get_ap());
+
+            let globals = PyDict::new(py);
+            globals
+                .set_item("memory", PyCell::new(py, memory).unwrap())
+                .unwrap();
+            globals
+                .set_item("ap", PyCell::new(py, ap).unwrap())
+                .unwrap();
+
+            // we try to insert to the same address two times
+            let code = r#"
+memory[ap] = 5
+memory[ap] = 3
+"#;
+
+            let py_result = py.run(code, Some(globals), None);
+
+            assert!(py_result.is_err());
+        });
+    }
+
+    #[test]
     fn memory_get_test() {
         Python::with_gil(|py| {
             let vm = PyVM::new(
@@ -93,11 +141,20 @@ mod test {
             let fp = PyRelocatable::from((1, 2));
 
             let globals = PyDict::new(py);
-            globals.set_item("memory", PyCell::new(py, memory).unwrap()).unwrap();
-            globals.set_item("ap", PyCell::new(py, ap).unwrap()).unwrap();
-            globals.set_item("fp", PyCell::new(py, fp).unwrap()).unwrap();
+            globals
+                .set_item("memory", PyCell::new(py, memory).unwrap())
+                .unwrap();
+            globals
+                .set_item("ap", PyCell::new(py, ap).unwrap())
+                .unwrap();
+            globals
+                .set_item("fp", PyCell::new(py, fp).unwrap())
+                .unwrap();
 
-            let code = "memory[ap] = 5";
+            let code = r#"
+memory[ap] = fp 
+assert memory[ap] == fp
+"#;
 
             let py_result = py.run(code, Some(globals), None);
 
