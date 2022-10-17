84934b43be0f7522c9521b9b9d69d6effc2b37ce
Federica Moletta <fedemoletta@hotmail.com>
2022-10-17 18:28:11 -0500

    Move cairo_run method

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,11 @@ mod scope_manager;
 mod utils;
 mod vm_core;
 
-use cairo_run::cairo_run_py;
 use pyo3::prelude::*;
 use vm_core::PyVM;
 
 #[pymodule]
 fn cairo_rs_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyVM>()?;
-    m.add_function(wrap_pyfunction!(cairo_run_py, m)?)?;
     Ok(())
 }
