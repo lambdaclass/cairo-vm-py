afdaa7995e29c44594694c10dcd9f43719822fd2
SantiagoPittella <pittellasantiago@gmail.com>
2022-10-17 17:13:01 -0500

    Add more tests

--- hints_tests.py
+++ hints_tests.py
@@ -1,8 +1,15 @@
 import cairo_rs_py
 
 def assert_not_zero_hint():
-    cairo_rs_py.cairo_run("cairo_programs/assert_not_zero.json", "main", False, False, None, None)
+    print(cairo_rs_py.cairo_run("cairo_programs/assert_not_zero.json", "main", False, False, None, None))
+def memory_add_hint():
+    print(cairo_rs_py.cairo_run("cairo_programs/memory_add.json", "main", False, False, None, None))
+
+def print_vars_hint():
+    print(cairo_rs_py.cairo_run("cairo_programs/hint_print_vars.json","main",False,False,None,None))
 
 if __name__ == "__main__":
     assert_not_zero_hint()
+    memory_add_hint()
+    print_vars_hint()
     print("ok")
