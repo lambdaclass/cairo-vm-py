1a1d3823842eed757b4a99e23f95815c18bd16f1
Pedro Fontana <pedro.fontana@lambdaclass.com>
2022-10-19 10:51:36 -0300

    add ec_mul_inner test

--- hints_tests.py
+++ hints_tests.py
@@ -8,8 +8,16 @@ def memory_add_hint():
 def print_vars_hint():
     print(cairo_rs_py.cairo_run("cairo_programs/hint_print_vars.json","main",False,False,None,None))
 
+def ec_mul_inner():
+    print(cairo_rs_py.cairo_run("cairo_programs/ec_mul_inner.json","main",False,False,None,None))
+
+def ec_negate():
+    print(cairo_rs_py.cairo_run("cairo_programs/ec_negate.json","main",False,False,None,None))
+
 if __name__ == "__main__":
     assert_not_zero_hint()
     memory_add_hint()
     print_vars_hint()
-    print("ok")
+    ec_mul_inner()
+    # ec_negate()
+    print("All test have passed")
