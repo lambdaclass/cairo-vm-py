15ca833b581389b6ce9ad2fc9343e7d879a746f4
Federica Moletta <fedemoletta@hotmail.com>
2022-10-20 18:50:10 -0300

    Add test for default_dict_new

---
+++ cairo_programs/default_dict_new.cairo
@@ -0,0 +1,19 @@
+from starkware.cairo.common.dict_access import DictAccess
+
+# Creates a new dictionary, with a default value.
+func default_dict_new(default_value : felt) -> (res : DictAccess*):
+    %{
+        if '__dict_manager' not in globals():
+            from starkware.cairo.common.dict import DictManager
+            __dict_manager = DictManager()
+
+        memory[ap] = __dict_manager.new_default_dict(segments, ids.default_value)
+    %}
+    ap += 1
+    return (res=cast([ap - 1], DictAccess*))
+end
+
+func main():
+    let my_dict = default_dict_new(17)
+    return ()
+end
--- hints_tests.py
+++ hints_tests.py
@@ -1,42 +1,45 @@
 import cairo_rs_py
 
 def assert_not_zero_hint():
-    print(cairo_rs_py.cairo_run("cairo_programs/assert_not_zero.json", "main", False, False, None, None))
+    print(cairo_rs_py.cairo_run("cairo_programs/assert_not_zero.json", "main", False, False))
 
 def memory_add_hint():
-    print(cairo_rs_py.cairo_run("cairo_programs/memory_add.json", "main", False, False, None, None))
+    print(cairo_rs_py.cairo_run("cairo_programs/memory_add.json", "main", False, False))
 
 def print_vars_hint():
-    print(cairo_rs_py.cairo_run("cairo_programs/hint_print_vars.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/hint_print_vars.json","main",False,False))
     
 def scope_hints():
-    print(cairo_rs_py.cairo_run("cairo_programs/vm_scope_hints.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/vm_scope_hints.json","main",False,False))
 
 def is_le_felt_hint():
-    print(cairo_rs_py.cairo_run("cairo_programs/is_le_felt_hint.json", "main", False, False, None, None))
+    print(cairo_rs_py.cairo_run("cairo_programs/is_le_felt_hint.json", "main", False, False))
 
 def assert_lt_felt_hint():
-    print(cairo_rs_py.cairo_run("cairo_programs/assert_le_felt_hint.json", "main", False, False, None, None))
+    print(cairo_rs_py.cairo_run("cairo_programs/assert_le_felt_hint.json", "main", False, False))
 
 def is_nn_hint():
-    print(cairo_rs_py.cairo_run("cairo_programs/is_nn_hint.json", "main", False, False, None, None))
+    print(cairo_rs_py.cairo_run("cairo_programs/is_nn_hint.json", "main", False, False))
 def ec_mul_inner():
-    print(cairo_rs_py.cairo_run("cairo_programs/ec_mul_inner.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/ec_mul_inner.json","main",False,False))
 
 def ec_negate():
-    print(cairo_rs_py.cairo_run("cairo_programs/ec_negate.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/ec_negate.json","main",False,False))
 
 def dict_new():
-    print(cairo_rs_py.cairo_run("cairo_programs/dict_new.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/dict_new.json","main",False,False))
 
 def dict_read():
-    print(cairo_rs_py.cairo_run("cairo_programs/dict_read.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/dict_read.json","main",False,False))
 
 def dict_write():
-    print(cairo_rs_py.cairo_run("cairo_programs/dict_write.json","main",False,False,None,None))
+    print(cairo_rs_py.cairo_run("cairo_programs/dict_write.json","main",False,False))
 
 def dict_update():
     print(cairo_rs_py.cairo_run("cairo_programs/dict_update.json","main",False,False,None,None))
+
+def default_dict_new():
+    print(cairo_rs_py.cairo_run("cairo_programs/default_dict_new.json","main",False,False))
     
 
 if __name__ == "__main__":
@@ -53,4 +56,5 @@ if __name__ == "__main__":
     #dict_read()
     #dict_write()
     #dict_update()
+    default_dict_new()
     print("All test have passed")
