e888380acc09d8fc9d735a5cb02c390196793852
Pedro Fontana <peter@MacBook-Air-de-Pedro.local>
2022-10-31 14:48:38 -0300

    uncomment tests

--- hints_tests.py
+++ hints_tests.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
     test_program("memcpy")
     test_program("memset")
     test_program("dict_new")
-    # test_program("dict_read") # ValueError: Custom Hint Error: AttributeError: 'PyTypeId' object has no attribute 'segment_index'
+    test_program("dict_read")
     # test_program("dict_write") # ValueError: Custom Hint Error: AttributeError: 'PyTypeId' object has no attribute 'segment_index'
-    # test_program("dict_update") # ValueError: Custom Hint Error: AttributeError: 'PyTypeId' object has no attribute 'segment_index'
+    test_program("dict_update")
     test_program("default_dict_new")
     # test_program("squash_dict") # ValueError: Custom Hint Error: ValueError: Failed to get ids value
     # test_program("dict_squash") # Custom Hint Error: AttributeError: 'PyTypeId' object has no attribute 'segment_index'
