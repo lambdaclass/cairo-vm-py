6c2d8c96ff24a882231cc1ca403e0567413c2614
Federica Moletta <fedemoletta@hotmail.com>
2022-11-01 14:16:21 -0300

    Fix compatibility

--- hints_tests.py
+++ hints_tests.py
@@ -1,7 +1,7 @@
 import cairo_rs_py
 
 def new_runner(program_name: str):
-    return cairo_rs_py.CairoRunner(f"cairo_programs/{program_name}.json", "main")
+    return cairo_rs_py.CairoRunner(f"cairo_programs/{program_name}.json", "main", "all")
 
 def test_program(program_name: str):
     print(new_runner(program_name).cairo_run(False))
