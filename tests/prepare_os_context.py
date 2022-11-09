cca703b16736384677b1580156a63975efd53a42
Milton <milton.scuderi@lambdaclass.com>
2022-11-09 16:56:04 -0300

    Add script to run tests

--- tests/prepare_os_context.py
+++ tests/prepare_os_context.py
@@ -15,3 +15,4 @@ if __name__ == "__main__":
     runner = init_runner("get_builtins_initial_stack")
     context = prepare_os_context(runner)
     assert str(context) == str([(0,0)])
+    print("prepare_os_context test passed")
