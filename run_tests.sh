d12c8fffcb15a460d5b150eaddb4af6177173aae
Milton <milton.scuderi@lambdaclass.com>
2022-11-10 12:01:15 -0300

    Move python tests to tests/ folder

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running hint tests"
-python3 hints_tests.py 
+python3 tests/hints_tests.py 
 python3 tests/get_builtins_initial_stack.py 
 
 echo "Running prepare_os_context integration tests"
--- tests/get_builtins_initial_stack.py
+++ tests/get_builtins_initial_stack.py
@@ -1,8 +1,9 @@
 import cairo_rs_py
 
 def new_runner(program_name: str):
-    return cairo_rs_py.CairoRunner(f"cairo_programs/{program_name}.json", "main", "all", False)
-
+    with open(f"cairo_programs/{program_name}.json") as file:
+        return cairo_rs_py.CairoRunner(file.read(), "main", "all", False)
+        
 def test_program(program_name: str):
     runner = new_runner(program_name)
     
