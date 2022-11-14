23f7e78728889fe85c437ea941fd98e4fb712df1
Federica Moletta <fedemoletta@hotmail.com>
2022-11-14 11:21:52 -0300

    Fix tests

--- src/cairo_runner.rs
+++ src/cairo_runner.rs
@@ -691,7 +691,7 @@ mod test {
         let mut runner = PyCairoRunner::new(
             program,
             Some("main".to_string()),
-            Some("all".to_string()),
+            Some("small".to_string()),
             false,
         )
         .unwrap();
@@ -710,20 +710,21 @@ mod test {
             .push((String::from("fake"), fake_builtin));
         // The fake builtin we added should be filtered out when getting the initial stacks,
         // so we should only get the range_check builtin's initial stack
-        let expected_output: Vec<PyMaybeRelocatable> = vec![RelocatableValue(PyRelocatable {
-            segment_index: 2,
-            offset: 0,
-        })];
+        let expected_output: Vec<Vec<PyMaybeRelocatable>> =
+            vec![vec![RelocatableValue(PyRelocatable {
+                segment_index: 2,
+                offset: 0,
+            })]];
 
         Python::with_gil(|py| {
             assert_eq!(
                 runner
                     .get_program_builtins_initial_stack(py)
-                    .extract::<Vec<PyMaybeRelocatable>>(py)
+                    .extract::<Vec<Vec<PyMaybeRelocatable>>>(py)
                     .unwrap(),
                 expected_output
-            );
-        });
+            )
+        })
     }
 
     #[test]
