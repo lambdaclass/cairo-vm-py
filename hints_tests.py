525c20070da57af1555954096e0467be64ce4882
Juan-M-V <juan.viel@lambdaclass.com>
2022-11-17 10:41:34 -0300

    Uncomment test

--- hints_tests.py
+++ hints_tests.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     test_program("memset")
     test_program("dict_new")
     test_program("dict_read")
-    #test_program("dict_write")
+    test_program("dict_write")
     test_program("dict_update")
     test_program("default_dict_new")
     # test_program("squash_dict") # ValueError: Custom Hint Error: ValueError: Failed to get ids value
