207bbbf96bcc52faa5d46566dee3dd7852789767
Juan Bono <juanbono94@gmail.com>
2022-11-17 17:45:24 -0300

    Return pyrelocatable from add_additional_hash_builtin (#149)
    * return a pyrelocatable
    
    * update test
    
    * check that it returns a pyrelocatable
    
    * Update src/cairo_runner.rs
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    * change Cargo.toml
    
    * fix test
    
    Co-authored-by: Juan Rigada <62958725+Jrigada@users.noreply.github.com>
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>

--- src/cairo_runner.rs
+++ src/cairo_runner.rs
@@ -536,9 +536,10 @@ impl PyCairoRunner {
     }
 
     /// Add (or replace if already present) a custom hash builtin.
-    pub fn add_additional_hash_builtin(&self) {
+    /// Returns a Relocatable with the new hash builtin base.
+    pub fn add_additional_hash_builtin(&self) -> PyRelocatable {
         let mut vm = (*self.pyvm.vm).borrow_mut();
-        self.inner.add_additional_hash_builtin(&mut vm);
+        self.inner.add_additional_hash_builtin(&mut vm).into()
     }
 }
 
@@ -1515,7 +1516,13 @@ mod test {
             )
             .unwrap();
 
-            runner.add_additional_hash_builtin();
+            let expected_relocatable = PyRelocatable {
+                segment_index: 0,
+                offset: 0,
+            };
+            let relocatable = runner.add_additional_hash_builtin();
+            assert_eq!(expected_relocatable, relocatable);
+
             assert_eq!(
                 (*runner.pyvm.vm)
                     .borrow()
