7ce46895d2180f361a17a692fb056242cbbe975c
Mario J. Rugiero <mrugiero@gmail.com>
2022-11-24 15:59:49 -0300

    Use whatever version we built

--- scripts/build_envs.sh
+++ scripts/build_envs.sh
@@ -4,7 +4,7 @@ python3.9 -m venv --upgrade-deps cairo-lang cairo-rs-py
 ./cairo-lang/bin/pip install cairo-lang==0.10.2
 ./cairo-rs-py/bin/pip install maturin==0.14.1 cairo-lang==0.10.2
 ./cairo-rs-py/bin/maturin build --manifest-path ../Cargo.toml --release --strip --interpreter 3.9 --no-default-features --features extension
-./cairo-rs-py/bin/pip install ../target/wheels/cairo_rs_py-0.1.0-cp39-cp39-manylinux_2_34_x86_64.whl
+./cairo-rs-py/bin/pip install ../target/wheels/cairo_rs_py-*.whl
 patch --directory ./cairo-rs-py/lib/python3.9/site-packages/ --strip 2 < move-to-cairo-rs-py.patch
 
 ./cairo-rs-py/bin/cairo-run --version
