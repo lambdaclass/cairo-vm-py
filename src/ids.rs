85a59ce24862625f7a04eb9175fe671e92233fb2
Tomás Grüner <tomas.gruner@lambdaclass.com>
2022-11-24 15:52:29 -0300

    Add tests for no register references

--- src/ids.rs
+++ src/ids.rs
@@ -1010,4 +1010,91 @@ memory[fp] = ids.ok_ref
             );
         });
     }
+
+    #[test]
+    fn ids_no_register_ref() {
+        Python::with_gil(|py| {
+            let vm = PyVM::new(
+                BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+                false,
+            );
+            for _ in 0..3 {
+                vm.vm.borrow_mut().add_memory_segment();
+            }
+            //Create references
+            let mut references = HashMap::new();
+            let imm = 89;
+            //Insert no register reference with immediate value
+            references.insert(
+                String::from("imm_ref"),
+                HintReference {
+                    register: None,
+                    offset1: 0,
+                    offset2: 0,
+                    dereference: true,
+                    inner_dereference: false,
+                    ap_tracking_data: None,
+                    immediate: Some(bigint!(imm)),
+                    cairo_type: None,
+                },
+            );
+            //Insert no register reference without imm
+            references.insert(
+                String::from("no_reg_ref"),
+                HintReference {
+                    register: None,
+                    offset1: 0,
+                    offset2: 0,
+                    dereference: true,
+                    inner_dereference: false,
+                    ap_tracking_data: None,
+                    immediate: None,
+                    cairo_type: None,
+                },
+            );
+
+            let memory = PyMemory::new(&vm);
+            let fp = PyRelocatable::from((1, 0));
+            let ids = PyIds::new(
+                &vm,
+                &references,
+                &ApTracking::default(),
+                &HashMap::new(),
+                Rc::new(HashMap::new()),
+            );
+
+            let globals = PyDict::new(py);
+            globals
+                .set_item("memory", PyCell::new(py, memory).unwrap())
+                .unwrap();
+            globals
+                .set_item("fp", PyCell::new(py, fp).unwrap())
+                .unwrap();
+            globals
+                .set_item("ids", PyCell::new(py, ids).unwrap())
+                .unwrap();
+
+            let code = r"memory[fp] = ids.imm_ref";
+
+            let py_result = py.run(code, Some(globals), None);
+
+            assert_eq!(py_result.map_err(to_vm_error), Ok(()));
+            //Check ids.a is now at memory[fp]
+            assert_eq!(
+                vm.vm.borrow().get_maybe(&Relocatable::from((1, 0))),
+                Ok(Some(MaybeRelocatable::from(bigint!(imm))))
+            );
+
+            let code = r"ids.no_reg_ref";
+
+            let py_result = py.run(code, Some(globals), None);
+
+            assert_eq!(
+                py_result.map_err(to_vm_error),
+                Err(to_vm_error(to_py_error(
+                    VirtualMachineError::NoRegisterInReference
+                )))
+            );
+        });
+    }
 }
