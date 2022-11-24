73e08189bce2ad96e19fff4224a9ef0af3b13c98
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2022-11-24 19:42:32 +0100

    Add `CairoRunner::vm_memory` property (alias of `memory`).

--- src/cairo_runner.rs
+++ src/cairo_runner.rs
@@ -597,6 +597,11 @@ impl PyCairoRunner {
     pub fn memory(&self) -> PyMemory {
         PyMemory::new(&self.pyvm)
     }
+
+    #[getter]
+    pub fn vm_memory(&self) -> PyMemory {
+        PyMemory::new(&self.pyvm)
+    }
 }
 
 #[pyclass]
@@ -1781,6 +1786,45 @@ mod test {
         let runner = PyCairoRunner::new(program, None, None, false).unwrap();
 
         let memory = runner.memory();
+
+        Python::with_gil(|py| {
+            let segment = runner.add_segment();
+
+            let set_value = |addr: &PyRelocatable, offset, value: BigInt| {
+                let addr = addr.__add__(offset);
+                memory
+                    .__setitem__(&addr, PyMaybeRelocatable::Int(value))
+                    .expect("Could not insert value into memory.");
+            };
+            let get_value = |addr: &PyRelocatable, offset| {
+                let addr = addr.__add__(offset);
+                memory
+                    .__getitem__(&addr, py)
+                    .expect("Could not get value from memory.")
+                    .map(|x| x.extract::<BigInt>(py))
+                    .transpose()
+                    .expect("Could not convert value to a BigInt")
+            };
+
+            set_value(&segment, 0, bigint!(1));
+            set_value(&segment, 1, bigint!(2));
+            set_value(&segment, 2, bigint!(3));
+            set_value(&segment, 3, bigint!(4));
+
+            assert_eq!(get_value(&segment, 0), Some(bigint!(1)));
+            assert_eq!(get_value(&segment, 1), Some(bigint!(2)));
+            assert_eq!(get_value(&segment, 2), Some(bigint!(3)));
+            assert_eq!(get_value(&segment, 3), Some(bigint!(4)));
+            assert_eq!(get_value(&segment, 4), None);
+        });
+    }
+
+    #[test]
+    fn vm_memory() {
+        let program = fs::read_to_string("cairo_programs/fibonacci.json").unwrap();
+        let runner = PyCairoRunner::new(program, None, None, false).unwrap();
+
+        let memory = runner.vm_memory();
 
         Python::with_gil(|py| {
             let segment = runner.add_segment();
