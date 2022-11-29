33d1b1eeee914b4a96e8e71cde0736c67d7f9926
Federica <federica@MacBook-Air-de-Federica.local>
2022-11-29 13:43:44 -0300

    Fix paths in build_envs script

--- scripts/memory_comparator/build_envs.sh
+++ scripts/memory_comparator/build_envs.sh
@@ -5,15 +5,15 @@ set -o pipefail
 
 # This is not reaaaaally a robust way to find it, but you need to be actively
 # trying to break it for this to fail :)
-SCRIPT_DIR=$(dirname $0)
+SCRIPT_DIR="scripts/memory_comparator"
 
 python3.9 -m venv --upgrade-deps ${SCRIPT_DIR}/cairo-lang ${SCRIPT_DIR}/cairo-rs-py
 ${SCRIPT_DIR}/cairo-lang/bin/pip install cairo-lang==0.10.1
 ${SCRIPT_DIR}/cairo-rs-py/bin/pip install maturin==0.14.1 cairo-lang==0.10.1
-${SCRIPT_DIR}/cairo-rs-py/bin/maturin build --manifest-path ${SCRIPT_DIR}/../Cargo.toml --release --strip --interpreter 3.9 --no-default-features --features extension
-${SCRIPT_DIR}/cairo-rs-py/bin/pip install ${SCRIPT_DIR}/../target/wheels/cairo_rs_py-*.whl
+${SCRIPT_DIR}/cairo-rs-py/bin/maturin build --manifest-path Cargo.toml --release --strip --interpreter 3.9 --no-default-features --features extension
+${SCRIPT_DIR}/cairo-rs-py/bin/pip install target/wheels/cairo_rs_py-*.whl
 patch --directory ${SCRIPT_DIR}/cairo-rs-py/lib/python3.9/site-packages/ --strip 2 < ${SCRIPT_DIR}/output-memory-cairo-rs-py.patch
-patch --directory ${SCRIPT_DIR}/cairo-langlib/python3.9/site-packages/ --strip 2 < ${SCRIPT_DIR}/output-memory-cairo-lang.patch
+patch --directory ${SCRIPT_DIR}/cairo-lang/lib/python3.9/site-packages/ --strip 2 < ${SCRIPT_DIR}/output-memory-cairo-lang.patch
 
 ${SCRIPT_DIR}/cairo-rs-py/bin/cairo-run --version
 ${SCRIPT_DIR}/cairo-rs-py/bin/starknet --version
