9b941b51ec52664ed577b16c497feef3434feb16
Federica <federica@MacBook-Air-de-Federica.local>
2022-12-01 12:49:47 -0300

    Replace memory_comparator

--- scripts/memory_comparator/run_tests_compare_memory.sh
+++ scripts/memory_comparator/run_tests_compare_memory.sh
@@ -5,7 +5,7 @@ exit_code=0
 cd starknet-devnet
 for file in test/test_*.py; do
     # Skip problematic files
-    if ! ([ "$file" = "test/test_account.py" ] || [ "$file" = "test/test_estimate_fee.py" ] || [ "$file" = "test/test_rpc_estimate_fee.py" ] || [ "$file" = "test/test_fee_token.py" ] || [ "$file" = "test/test_postman.py" ] || [ "$file" = "test/testnet_deployment.py" ]); then
+    if ! ([ "$file" = "test/test_account.py" ] || [ "$file" = "test/test_estimate_fee.py" ] || [ "$file" = "test/test_rpc_estimate_fee.py" ] || [ "$file" = "test/test_fee_token.py" ] || [ "$file" = "test/test_postman.py" ] || [ "$file" = "test/testnet_deployment.py" ] || [ "$file" = "test/test_api_specifications.py" ]); then
         # Run tests in cairo-rs-py env
         . ../scripts/memory_comparator/cairo-rs-py/bin/activate
         poetry run pytest $file
@@ -15,7 +15,7 @@ for file in test/test_*.py; do
         # Compare memory outputs
         class_hash_path="memory_files/class_hash"
         execute_entry_point_path="memory_files/execute_entry_point"
-        memory_comparator_path="../tests/memory_comparator.py"
+        memory_comparator_path="../scripts/memory_comparator/memory_comparator.py"
         if ! $memory_comparator_path $class_hash_path.memory $class_hash_path.rs.memory; then
             echo "Memory differs for last class_hash on test $file"
             exit_code=1
