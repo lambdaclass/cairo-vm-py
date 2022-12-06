5483aa6ee5b68bf2d2f80bbbab9efbd4ffeb6cc4
daphneherlambda <daphne.hernandez@lambdaclass>
2022-12-06 13:00:06 -0500

    add lib.rs test

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,17 @@ fn cairo_rs_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyCairoRunner>()?;
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use pyo3::prelude::*;
+    use pyo3::Python;
+
+    #[test]
+    fn cairo_rs_py_test() {
+        Python::with_gil(|py| {
+            let module = PyModule::new(py, "My Module");
+            crate::cairo_rs_py(py, module.unwrap());
+        });
+    }
+}
