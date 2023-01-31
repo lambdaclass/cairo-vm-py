6c7f54124f2b627f504fab7dd33f7e5154211bac
Mario Rugiero <mrugiero@gmail.com>
2023-01-31 16:05:06 +0200

    cargo fmt

--- src/ids.rs
+++ src/ids.rs
@@ -218,9 +218,7 @@ impl PyTypedId {
             .ok_or_else(|| PyValueError::new_err(STRUCT_TYPES_GET_ERROR_MSG))?;
 
         let member = struct_type.get(field_name).ok_or_else(|| {
-            PyAttributeError::new_err(format!(
-                "'PyTypeId' object has no attribute '{field_name}'"
-            ))
+            PyAttributeError::new_err(format!("'PyTypeId' object has no attribute '{field_name}'"))
         })?;
 
         let mut vm = self.vm.borrow_mut();
